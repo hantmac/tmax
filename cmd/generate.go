8a39c09e0f847389ed0358583ae0c2b1266c91f6
hantmac <hantmac@outlook.com>
2021-01-09 15:48:02 +0800

    add more cmd
    Signed-off-by: hantmac <hantmac@outlook.com>

--- cmd/generate.go
+++ cmd/generate.go
@@ -24,26 +24,6 @@ import (
 	"tmax/setting"
 )
 
-var tmaxDefaultConf = `
----
-k8s:
-- filternodecpu: kubectl get nodes -o=jsonpath="{range .items[*]}{.metadata.name}{'\t'}{.status.capacity.cpu}{'\t'}{.status.capacity.memory}{'\n'}{end}"
-- filternodetaint: kubectl get nodes -o=jsonpath="{range .items[*]}{.metadata.name}{'\t'}{.spec.taints[*].key}{'\n'}{end}"
-- corednsedit: kubectl edit cm coredns -nkube-system
-- allnode: kubectl get no
-- alldeploy: kubectl get deploy
-- allpod: kubectl get pod -A
-- getns: kubectl get ns
-- busyboxrun: kubectl run busybox --rm -ti --image=busybox /bin/sh
-- allnodeip: kubectl get node -o=jsonpath="{range .items[*]}{.metadata.name}{'\t'}{.status.addresses[0].address}{'\n'}{end}"
-- podResource: kubectl get pod -o custom-columns=NAME:metadata.name,podIP:status.podIP,hostIp:spec.containers[0].resources | grep 8Gi
-custom:
-- check: curl 127.0.0.1:8080/@in/api/health
-unix:
-- 'tar': tar -xvjf test.tar.bz2
-- 'netutils': yum install net-tools -y
-`
-
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
@@ -78,7 +58,7 @@ func GenerateTmaxYaml() {
 		log.Errorf("open tmax.yaml failed")
 		return
 	} else {
-		_, err = f.Write([]byte(tmaxDefaultConf))
+		_, err = f.Write([]byte(setting.TmaxDefaultConf))
 	}
 	defer f.Close()
 
